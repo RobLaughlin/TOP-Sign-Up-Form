5c6ece08db752f29a9bfaf90b539909f0d96d235
Rob Laughlin <waterfallkicks@gmail.com>
2024-08-09 03:41:00 -0700

    Update README.md and add basic phone number validation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# TOP-Sign-Up-Form
+# TOP Sign Up Form
 The sign up form assignment for The Odin Project.
+
+## Live Demo
+A live demo of the page can be found [here](https://roblaughlin.github.io/TOP-Sign-Up-Form/).
--- js/index.js
+++ js/index.js
@@ -20,7 +20,27 @@ function pwdFieldChanged(e) {
     }
 }
 
+function handlePhoneNumberInput(e) {
+    if (e.key === 'Backspace' && e.target.value.length >= 2) {
+        if (e.target.value[e.target.value.length - 2] === '-') {
+            e.target.value = e.target.value.substring(0, e.target.value.length-1);
+        }
+    }
+    else if (e.key < '0' || e.key > '9') {
+        e.preventDefault();
+    }
+    else {
+        // When to add a hyphen
+        if (e.target.value.length === 3 || e.target.value.length === 7) {
+            e.target.value += '-';
+        }
+    }
+}
+
 const pwdText = document.getElementById("Password");
 const confirmPwdText = document.getElementById("ConfirmPassword");
+const phoneNumberText = document.getElementById("PhoneNumber");
+
 pwdText.addEventListener("input", pwdFieldChanged);
 confirmPwdText.addEventListener("input", pwdFieldChanged);
+phoneNumberText.addEventListener("keydown", handlePhoneNumberInput);
